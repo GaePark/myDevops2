532bdd7b9145a6f48cb0c4ddaea480c11d542403
GaePark <qkrwnsgus01@gmail.com>
2025-06-16 14:50:29 +0900

    20250616

---
+++ src/main/java/com/sist/web/MyDevops2Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevops2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDevops2Application.class, args);
+	}
+
+}
